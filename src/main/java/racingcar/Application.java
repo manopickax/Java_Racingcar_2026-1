57db6ec8c9dcc906c0a1e0cbc0cf498143b19453
manopickax <rnrrbs0202@gmail.com>
2026-03-30 00:08:56 +0900

    feat: GameManager 클래스 기능 추가 및 예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -6,28 +6,15 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Application {
-
     public static void main(String[] args) {
-        List<Car> cars = createCars();
-    }
-
-    private static List<Car> createCars() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String input = Console.readLine();
-        String[] names = input.split(",");
-
-        List<Car> cars = new ArrayList<>();
-        for (String name : names) {
-            cars.add(new Car(name.trim()));
-        }
-        return cars;
+        GameManager gameManager = new GameManager();
+        gameManager.start();
     }
 }
 
 class Car {
     private final String name;
     private int distance = 0;
-    int randomNumber = Randoms.pickNumberInRange(0, 9);
 
     public Car (String name) {
         validateName(name);
@@ -43,5 +30,43 @@ class Car {
             distance ++;
         }
     }
+    public void printStatus() {
+        System.out.println(name + " : " + " - ".repeat(distance));
+    }
+}
+
+class GameManager {
+    public void start() {
+        List<Car> cars = createCars();
+        System.out.println("시도할 회수");
+        String inputNum = Console.readLine();
+        int attempts = Integer.parseInt(inputNum);
+        if (attempts <= 0 ) {
+            throw new IllegalArgumentException ("0이상 입력하세요.");
+        }
+        for(int i = 1; i <= attempts; i++){
+            int randomNumber = Randoms.pickNumberInRange(0, 9);
+            for (Car car : cars){
+                car.move(randomNumber);
+                car.printStatus();
+            }
+            System.out.println(" ");
+        }
+
+    }
+    private static List<Car> createCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String inputName = Console.readLine();
+        String[] names = inputName.split(",");
+
+        List<Car> cars = new ArrayList<>();
+        for (String name : names) {
+            cars.add(new Car(name.trim()));
+        }
+        return cars;
+    }
+}
+
+class WinnerPicker {
 
 }
