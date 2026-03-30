c2166403156a08c9f3d24c028b7f35c9cfc3c739
manopickax <rnrrbs0202@gmail.com>
2026-03-30 13:06:39 +0900

    feat: WinnerPicker 클래스 기능 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,6 @@
 package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.List;
 import java.util.ArrayList;
 
@@ -33,26 +32,34 @@ class Car {
     public void printStatus() {
         System.out.println(name + " : " + " - ".repeat(distance));
     }
+    public int getDistance() {
+        return distance;
+    }
+    public String getName() {
+        return name;
+    }
+
 }
 
 class GameManager {
     public void start() {
         List<Car> cars = createCars();
-        System.out.println("시도할 회수");
+        System.out.println("시도할 회수는 몇회인가요?");
         String inputNum = Console.readLine();
         int attempts = Integer.parseInt(inputNum);
         if (attempts <= 0 ) {
             throw new IllegalArgumentException ("0이상 입력하세요.");
         }
         for(int i = 1; i <= attempts; i++){
-            int randomNumber = Randoms.pickNumberInRange(0, 9);
             for (Car car : cars){
+                int randomNumber = Randoms.pickNumberInRange(0, 9);
                 car.move(randomNumber);
                 car.printStatus();
             }
             System.out.println(" ");
         }
-
+        WinnerPicker winnerPicker = new WinnerPicker();
+        winnerPicker.pirntWinner(cars);
     }
     private static List<Car> createCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -68,5 +75,19 @@ class GameManager {
 }
 
 class WinnerPicker {
-
+    public void pirntWinner(List<Car> cars) {
+        int maxdistance = 0;
+        for (Car car : cars) {
+            if (car.getDistance() > maxdistance) {
+                maxdistance = car.getDistance();
+            }
+        }
+        List<String> winnercars = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getDistance() == maxdistance) {
+                winnercars.add(car.getName());
+            }
+        }
+        System.out.println(winnercars);
+    }
 }
