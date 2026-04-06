6bdc45d310d18eda46763828314042c983912cf0
manopickax <rnrrbs0202@gmail.com>
2026-04-06 22:21:58 +0900

    refactor: SRP원칙 준수 고려 코드 재구성

--- src/main/java/racingcar/GameManager.java
+++ src/main/java/racingcar/GameManager.java
@@ -1,18 +1,13 @@
 package racingcar;
 
-import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class GameManager {
     public void start() {
         List<Car> cars = CarCreator.createCars();
-        System.out.println("시도할 회수는 몇 회인가요?");
-        String inputNum = Console.readLine();
-        int attempts = Integer.parseInt(inputNum);
-        if (attempts <= 0 ) {
-            throw new IllegalArgumentException ("0이상 입력하세요.");
-        }
+        int attempts = AttemptReader.read();
+
         System.out.println();
         System.out.println("실행 결과");
         for(int i = 1; i <= attempts; i++){
